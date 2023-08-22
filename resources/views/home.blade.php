241a95eef2549e681037b627288ca2b731052e60
Andrea Creazzi <andreacreazzi@libero.it>
2023-08-22 14:51:42 +0200

    add home.blade.php + encounter strange error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                <p>{{ $movie->title }}</p>
+            </li>
+        @endforeach
+    </ul>
+
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }} | @yield('title')</title>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-    <header></header>
+    <header>
+        @include('includes.header')
+    </header>
     <main>
         @yield('content')
     </main>
